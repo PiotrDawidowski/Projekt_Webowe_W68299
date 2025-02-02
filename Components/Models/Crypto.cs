466f6fc2cb4add22a15eaaca0bcfa78de637f41f
Piotr Dawidowski <pepedawidowski@gmail.com>
2025-02-02 01:10:52 +0100

    Dodaj pliki projektów.

---
+++ Components/Models/Crypto.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace ProjektKrypto2.Components.Models
+{
+    public class Crypto
+    {
+        [JsonPropertyName("id")]
+        public string Id { get; set; } = string.Empty;
+
+        [JsonPropertyName("rank")]
+        public string Rank { get; set; } = string.Empty;
+
+        [JsonPropertyName("symbol")]
+        public string Symbol { get; set; } = string.Empty;
+
+        [JsonPropertyName("name")]
+        public string Name { get; set; } = string.Empty;
+
+        [JsonPropertyName("priceUsd")]
+        public string PriceUsd { get; set; } = "0";
+
+        [JsonPropertyName("changePercent24Hr")]
+        public string ChangePercent24Hr { get; set; } = "0";
+
+        [JsonPropertyName("volumeUsd24Hr")] // ✅ Dodane, aby naprawić błąd CS1061!
+        public string VolumeUsd24Hr { get; set; } = "0";
+
+        public string GetIconUrl()
+        {
+            return $"https://assets.coincap.io/assets/icons/{Symbol.ToLower()}@2x.png";
+        }
+    }
+
+    public class CryptoResponse
+    {
+        [JsonPropertyName("data")]
+        public List<Crypto> Data { get; set; } = new();
+    }
+}
---
+++ Components/Models/CryptoComparer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ProjektKrypto2.Components.Models
+{
+    public class CryptoComparer : IEqualityComparer<Crypto>
+    {
+        public bool Equals(Crypto? x, Crypto? y) // ✅ Obsługa wartości null, aby naprawić CS8767
+        {
+            if (x is null || y is null)
+                return false;
+
+            return x.Symbol == y.Symbol && x.PriceUsd == y.PriceUsd;
+        }
+
+        public int GetHashCode(Crypto obj)
+        {
+            return HashCode.Combine(obj.Symbol, obj.PriceUsd);
+        }
+    }
+}
